5a2895014efdb50abe4c63e2d63fbea84e74db56
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-04-01 10:03:38 -0700

    Added Virtual ATM

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to virtual-atm-ts<br>
+**virtual-atm-ts** is a TypeScript-based virtual ATM simulator designed to mimic real-world banking operations.<br>
+<br>
+**Features:**<br>
+- Simulate ATM operations including withdrawals, balance inquiries, and fast cash transactions.<br>
+- Built with TypeScript for enhanced code readability and maintainability.<br>
+- Utilizes Inquirer for interactive command-line prompts.<br>
+- Enhances user experience with colorful output using the Chalk library.<br>
+<br>
+**Getting Started:**<br>
+1. Install the virtual-atm-ts package globally using `npm install -g chalk-k7-atm-machine`.<br>
+2. Run the virtual ATM simulator using the command `chalk-k7-atm-machine`.<br>
+3. Follow the on-screen instructions to navigate through the virtual ATM.<br>
+<br>
+Thank you for choosing virtual-atm-ts. Have fun exploring! 💳💰<br>
---
+++ main.js
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+let myBalance = 20000;
+let myPin = 77550;
+console.log(chalk.magentaBright('Welcome to the ATM :'));
+const pinAns = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'pin',
+        message: (chalk.grey('Enter Your Pin :')),
+    }
+]);
+if (pinAns.pin !== myPin) {
+    console.log(chalk.redBright("Incorrect pin !"));
+}
+else {
+    console.log(chalk.green("Succesfully login !"));
+    while (true) {
+        let operation = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selectOpr',
+                message: (chalk.grey('What you want to do ?')),
+                choices: [chalk.blue("Withdraw"), chalk.blue("Current Balance"), chalk.blue("Fast Cash"), chalk.red("Exit")]
+            }
+        ]);
+        if (operation.selectOpr === chalk.blue("Withdraw")) {
+            const amount = await inquirer.prompt([
+                {
+                    type: 'number',
+                    name: 'enterAmount',
+                    message: (chalk.grey('Enter amount you want to withdraw :'))
+                }
+            ]);
+            let remainig = myBalance - amount.enterAmount;
+            if (amount.enterAmount > myBalance) {
+                console.log(chalk.redBright('Insufficient Balance !'));
+            }
+            else if (amount.enterAmount <= myBalance) {
+                console.log(chalk.green(`Withdraw successfull! your remaining Balance is : ${remainig}`));
+            }
+            else {
+                console.log(chalk.redBright('Invalid Number'));
+            }
+        }
+        else if (operation.selectOpr === chalk.blue("Current Balance")) {
+            console.log(chalk.magenta(`Your Current Balance is : ${myBalance}`));
+        }
+        else if (operation.selectOpr === chalk.blue("Fast Cash")) {
+            const fastAmount = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'amount',
+                    messsage: (chalk.grey('select Fast Cash Amount ?')),
+                    choices: [5000, 10000, 15000, 20000]
+                }
+            ]);
+            let withdrawAmount = fastAmount.amount;
+            if (withdrawAmount <= myBalance) {
+                myBalance -= withdrawAmount;
+                console.log(chalk.green(`Fast Cash withdrawal of ${withdrawAmount} successful! Your remaining balance is: ${myBalance}`));
+            }
+            else {
+                console.log(chalk.redBright('Insufficient balance for fast cash withdrawal!'));
+            }
+        }
+        else {
+            console.log(chalk.yellow('Thank you for using ATM !'));
+            break;
+        }
+        ;
+    }
+}
